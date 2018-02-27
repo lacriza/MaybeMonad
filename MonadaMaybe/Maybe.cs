4592f1566e70531c7c6fae34b31fa6f360ca5cf6
Ada Saliy <a0676001993@gmail.com>
2018-02-27 14:35:46 +0200

    remove

---
+++ MonadaMaybe/Maybe.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace MonadaMaybe
+{
+
+	public static class Maybe
+	{
+
+		public static TResult With<TInput, TResult>(this TInput obj, Func<TInput, TResult> evaluator)
+			where TInput : class
+			where TResult : class
+		{
+			if (obj == null) return null;
+			return evaluator(obj);
+		}
+
+		public static TResult Return<TInput, TResult>(this TInput obj, Func<TInput, TResult> evaluator, TResult failureValue)
+			where TInput : class
+		{
+			if (obj == null) return failureValue;
+			return evaluator(obj);
+		}
+
+		public static bool ReturnSuccess<TInput>(this TInput obj)
+			where TInput : class
+		{
+			return obj != null;
+		}
+
+		public static TInput If<TInput>(this TInput obj, Predicate<TInput> evaluator)
+			where TInput : class
+		{
+			if (obj == null) return null;
+			return evaluator(obj) ? obj : null;
+		}
+
+		public static TInput Do<TInput>(this TInput obj, Action<TInput> action)
+			where TInput : class
+		{
+			if (obj == null) return null;
+			action(obj);
+			return obj;
+		}
+
+	}
+}
